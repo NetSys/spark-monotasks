f232eb15bd502e2ae28e8b06bf9a0ef63d7296f3
Kay Ousterhout <kayousterhout@gmail.com>
2014-06-29 14:56:57 -0700

    Add per-task utilization information.
    Thiis commit adds instrumentation for CPU, disk, and network utilization.
    The instrumentation is included in the metrics sent back to the driver
    about each task, and also is logged every 10ms (by default) on each
    executor, so that we can measure how utilization changes with time.
    
    Conflicts:
    	core/src/main/scala/org/apache/spark/executor/Executor.scala
    	core/src/main/scala/org/apache/spark/executor/TaskMetrics.scala
    	core/src/main/scala/org/apache/spark/util/JsonProtocol.scala
    
    Conflicts:
    	core/src/main/scala/org/apache/spark/executor/Executor.scala
    	core/src/main/scala/org/apache/spark/executor/TaskMetrics.scala

---
+++ core/src/main/scala/org/apache/spark/performance_logging/CpuCounters.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2014 The Regents of The University California
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.performance_logging
+
+import java.io.FileNotFoundException
+
+import scala.io.Source
+
+import org.apache.spark.Logging
+
+class CpuCounters() extends Serializable with Logging {
+  val timeMillis = System.currentTimeMillis()
+
+  // Total CPU time used by the Spark process.
+  var processUserJiffies = 0L
+  var processSystemJiffies = 0L
+
+  // Total CPU time used by all processes on the machine.
+  var totalUserJiffies = 0L
+  var totalSystemJiffies = 0L
+
+  try {
+    Source.fromFile(s"/proc/${Utils.getPid()}/stat").getLines().foreach { line =>
+      val values = line.split(" ")
+      processUserJiffies = values(CpuCounters.UTIME_INDEX).toLong
+      processSystemJiffies = values(CpuCounters.STIME_INDEX).toLong
+    }
+
+    Source.fromFile(CpuCounters.CPU_TOTALS_FILENAME).getLines().foreach { line =>
+      // Look for only the line that starts with "cpu  ", which has the totals across all CPUs
+      // (the remaining lines are for a particular core).
+      if (line.startsWith("cpu ")) {
+        val cpuTimes =
+          line.substring(CpuCounters.CPU_COUNTS_START_INDEX, line.length).split(" ").map(_.toInt)
+        totalUserJiffies = cpuTimes(CpuCounters.USER_JIFFIES_INDEX)
+        totalSystemJiffies = cpuTimes(CpuCounters.SYSTEM_JIFFIES_INDEX)
+      }
+    }
+  } catch {
+    case e: FileNotFoundException =>
+      logWarning(
+        "Unable to record CPU counters because files in /proc filesystem could not be found")
+  }
+}
+
+object CpuCounters {
+  val CPU_TOTALS_FILENAME = "/proc/stat"
+
+  // The first 5 characters in /proc/stat are "cpu  " or "cpuX " where X is the indentifier of the
+  // particular core.
+  val CPU_COUNTS_START_INDEX = 5
+
+  // Indexes of the counters for user / system CPU counters, starting after the CPU identifier.
+  val USER_JIFFIES_INDEX = 0
+  val SYSTEM_JIFFIES_INDEX = 2
+
+  // 0-based index in /proc/pid/stat file of the user CPU time. Not necessarily portable.
+  val UTIME_INDEX = 13
+  val STIME_INDEX = 14
+}
---
+++ core/src/main/scala/org/apache/spark/performance_logging/DiskUtilization.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2014 The Regents of The University California
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.performance_logging
+
+import scala.collection.mutable.HashMap
+
+/** Utilization of a particular block device. */
+class BlockDeviceUtilization(
+    startCounters: BlockDeviceCounters,
+    endCounters: BlockDeviceCounters,
+    elapsedMillis: Long) extends Serializable {
+  val diskUtilization = ((endCounters.millisTotal - startCounters.millisTotal).toFloat /
+    elapsedMillis)
+  val readThroughput = ((endCounters.sectorsRead - startCounters.sectorsRead).toFloat *
+    DiskUtilization.SECTOR_SIZE_BYTES * 1000 / elapsedMillis)
+  val writeThroughput = ((endCounters.sectorsWritten - startCounters.sectorsWritten).toFloat *
+    DiskUtilization.SECTOR_SIZE_BYTES * 1000 / elapsedMillis)
+}
+
+class DiskUtilization(startCounters: DiskCounters, endCounters: DiskCounters) extends Serializable {
+  val deviceNameToUtilization = HashMap[String, BlockDeviceUtilization]()
+  val elapsedMillis = endCounters.timeMillis - startCounters.timeMillis
+  endCounters.deviceNameToCounters.foreach {
+    case (deviceName: String, endCounters: BlockDeviceCounters) =>
+      startCounters.deviceNameToCounters.get(deviceName).foreach {
+        deviceNameToUtilization +=
+          deviceName -> new BlockDeviceUtilization(_, endCounters, elapsedMillis)
+      }
+  }
+
+  def this(startCounters: DiskCounters) = this(startCounters, new DiskCounters())
+}
+
+object DiskUtilization {
+  // This is not at all portable -- can be obtained for a particular machine with "fdisk -l".
+  val SECTOR_SIZE_BYTES = 512
+}
