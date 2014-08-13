792e04d38c6549aa54d655dc8b3e45d566e56d9a
Ryan Rasti <ryanrasti@gmail.com>
2014-08-12 23:09:21 -0700

    stash: debugging -> looks like MiniFetchDependency is not getting properly serialized (unlike its superclass) and aggregator is not found for it ...

--- core/src/main/scala/org/apache/spark/rdd/MiniFetchRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/MiniFetchRDD.scala
@@ -2,6 +2,8 @@ package org.apache.spark.rdd
 
 import org.apache.spark._
 import org.apache.spark.storage.{ShuffleBlockId}
+import org.apache.spark.util.collection.ExternalSorter
+import org.apache.spark.serializer.Serializer
 
 /**
  * Like ShuffledRDD, but instead requires Fetch mini-tasks to first fetch all parts of
@@ -19,9 +21,45 @@ class MiniFetchRDD[K, V, C](prev: RDD[_ <: Product2[K, V]], part: Partitioner)
    * compute from a downstream task and it should behave just like a ShuffledRDD
    */
   override def compute(split: Partition, context: TaskContext): Iterator[(K, C)] = {
-    shuffleBlockIdsByPartition(split.index).flatMap {
+    val iter = shuffleBlockIdsByPartition(split.index).flatMap {
       SparkEnv.get.blockManager.memoryStore.getValues(_).get
-    }.toIterator.asInstanceOf[Iterator[(K, C)]]
+    }.toIterator.asInstanceOf[Iterator[Product2[K, C]]]
+    aggregateAndStuff(iter, context).asInstanceOf[Iterator[(K, C)]]
+  }
+
+  // TODO(ryan): this is copied from HashShuffleReader (it doesn't seem easy to refactor
+  // that code to avoid the copy paste)
+  private def aggregateAndStuff(iter: Iterator[Product2[K, C]],
+                                context: TaskContext): Iterator[Product2[K, C]] = {
+    val dep = dependencies.head.asInstanceOf[ShuffleDependency[K, V, C]]
+    val ser = Serializer.getSerializer(dep.serializer)
+    val aggregatedIter: Iterator[Product2[K, C]] = if (dep.aggregator.isDefined) {
+      if (dep.mapSideCombine) {
+        new InterruptibleIterator(context, dep.aggregator.get.combineCombinersByKey(iter, context))
+      } else {
+        new InterruptibleIterator(context,
+          dep.aggregator.get.combineValuesByKey(iter.asInstanceOf[Iterator[Product2[K, V]]], context))
+      }
+    } else if (dep.aggregator.isEmpty && dep.mapSideCombine) {
+      throw new IllegalStateException("Aggregator is empty for map-side combine")
+    } else {
+      // Convert the Product2s to pairs since this is what downstream RDDs currently expect
+      iter.asInstanceOf[Iterator[Product2[K, C]]].map(pair => (pair._1, pair._2))
+    }
+
+    // Sort the output if there is a sort ordering defined.
+    dep.keyOrdering match {
+      case Some(keyOrd: Ordering[K]) =>
+        // Create an ExternalSorter to sort the data. Note that if spark.shuffle.spill is disabled,
+        // the ExternalSorter won't spill to disk.
+        val sorter = new ExternalSorter[K, C, C](ordering = Some(keyOrd), serializer = Some(ser))
+        sorter.write(aggregatedIter)
+        context.taskMetrics.memoryBytesSpilled += sorter.memoryBytesSpilled
+        context.taskMetrics.diskBytesSpilled += sorter.diskBytesSpilled
+        sorter.iterator
+      case None =>
+        aggregatedIter
+    }
   }
 
   val shuffleBlockIdsByPartition: Map[Int, Seq[ShuffleBlockId]] = {
