c70691e7a51695add3153b97410ba54df2e841dc
Kay Ousterhout <kayousterhout@gmail.com>
2014-10-14 17:45:55 -0700

    Store shuffle data in-memory via the block mgr.
    As-is, shuffle data will not be deleted from the block
    manager until the RDD goes out of scope. We need to add
    more aggressive cleaning for this, which is necessary
    now that data is stored in-memory.

--- core/src/main/scala/org/apache/spark/SparkEnv.scala
+++ core/src/main/scala/org/apache/spark/SparkEnv.scala
@@ -287,8 +287,9 @@ object SparkEnv extends Logging {
     // Let the user specify short names for shuffle managers
     val shortShuffleMgrNames = Map(
       "hash" -> "org.apache.spark.shuffle.hash.HashShuffleManager",
+      "memory" -> "org.apache.spark.shuffle.memory.MemoryShuffleManager",
       "sort" -> "org.apache.spark.shuffle.sort.SortShuffleManager")
-    val shuffleMgrName = conf.get("spark.shuffle.manager", "sort")
+    val shuffleMgrName = conf.get("spark.shuffle.manager", "memory")
     val shuffleMgrClass = shortShuffleMgrNames.getOrElse(shuffleMgrName.toLowerCase, shuffleMgrName)
     val shuffleManager = instantiateClass[ShuffleManager](shuffleMgrClass)
 
