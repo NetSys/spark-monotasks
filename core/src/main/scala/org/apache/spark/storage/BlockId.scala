aee1ac77924d4346340405d651cc18ee543985c3
Christopher Canel <christopher.canel@icloud.com>
2015-02-23 15:52:15 -0800

    Fixed bug in MonotaskResultBlockId implementation.
    The BlockId singleton object did not contain handling for the
    MonotaskResultBlockId class. This caused an error when trying to create
    a MonotaskResultBlockId object by parsing its String representation.
    Added the necessary handling to fix this bug.

--- core/src/main/scala/org/apache/spark/storage/BlockId.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockId.scala
@@ -15,6 +15,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2014 The Regents of The University California
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.storage
 
 import java.util.UUID
@@ -95,6 +111,7 @@ object BlockId {
   val SHUFFLE_INDEX = "shuffle_([0-9]+)_([0-9]+)_([0-9]+).index".r
   val BROADCAST = "broadcast_([0-9]+)([_A-Za-z0-9]*)".r
   val TASKRESULT = "taskresult_([0-9]+)".r
+  val MONOTASKRESULT = "monotaskresult_([0-9]+)".r
   val STREAM = "input-([0-9]+)-([0-9]+)".r
   val TEST = "test_(.*)".r
 
@@ -110,6 +127,8 @@ object BlockId {
       BroadcastBlockId(broadcastId.toLong, field.stripPrefix("_"))
     case TASKRESULT(taskId) =>
       TaskResultBlockId(taskId.toLong)
+    case MONOTASKRESULT(monotaskId) =>
+      MonotaskResultBlockId(monotaskId.toLong)
     case STREAM(streamId, uniqueId) =>
       StreamBlockId(streamId.toInt, uniqueId.toLong)
     case TEST(value) =>
