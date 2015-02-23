42f41bd49579ab9ae94e33a45486c42a61cb744e
Christopher Canel <christopher.canel@icloud.com>
2015-02-23 15:52:15 -0800

    Fixed bug in MonotaskResultBlockId implementation.
    The BlockId singleton object did not contain handling for the
    MonotaskResultBlockId class. This caused an error when trying to create
    a MonotaskResultBlockId object by parsing its String representation.
    Added the necessary handling to fix this bug.

--- core/src/test/scala/org/apache/spark/storage/BlockIdSuite.scala
+++ core/src/test/scala/org/apache/spark/storage/BlockIdSuite.scala
@@ -15,6 +15,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2014 The Regents of The University California
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.storage
 
 import org.scalatest.FunSuite
@@ -85,6 +101,17 @@ class BlockIdSuite extends FunSuite {
     assert(id.name === "taskresult_60")
     assert(id.asRDDId === None)
     assert(id.taskId === 60)
+    assert(!id.isRDD)
+    assertSame(id, BlockId(id.toString))
+  }
+
+  test("monotaskresult") {
+    val id = MonotaskResultBlockId(60)
+    assertSame(id, MonotaskResultBlockId(60))
+    assertDifferent(id, MonotaskResultBlockId(61))
+    assert(id.name === "monotaskresult_60")
+    assert(id.asRDDId === None)
+    assert(id.monotaskId === 60)
     assert(!id.isRDD)
     assertSame(id, BlockId(id.toString))
   }
