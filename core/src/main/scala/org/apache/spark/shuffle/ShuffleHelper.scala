48b52465718ac24c220e36078ff4414990e36c78
Kay Ousterhout <kayousterhout@gmail.com>
2015-07-16 23:02:20 -0700

    Add ability to read local shuffle data from disk.
    The commit changes ShuffleHelper to create DiskReadMonotasks to read local
    shuffle blocks, in addition to the NetworkMonotasks it creates to read remote
    shuffle blocks.
    
    With this commit, doing a shuffle when running the local version of Spark
    works, but doing a shuffle when Spark is distributed still doesn't.

--- core/src/main/scala/org/apache/spark/shuffle/ShuffleHelper.scala
+++ core/src/main/scala/org/apache/spark/shuffle/ShuffleHelper.scala
@@ -28,13 +28,14 @@ import org.apache.spark.storage.{BlockId, BlockManagerId, MonotaskResultBlockId,
 import org.apache.spark.util.CompletionIterator
 
 /**
- * Handles creating network monotasks to read shuffle data over the network. Also provides
- * functionality for deserializing, aggregating, and sorting the result.
+ * Handles creating network monotasks to read shuffle data over the network and disk monotasks to
+ * read shuffle data stored on local disks. Also provides functionality for deserializing,
+ * aggregating, and sorting the result.
  *
  * Each task that performs a shuffle should create one instance of this class.  The class stores
  * the intermediate block IDs used to store the shuffle data in the memory store after network
- * monotasks have read it over the network.  These IDs are used to later read the data (by the
- * compute monotasks that perform the computation).
+ * monotasks have read it over the network and disk monotasks have read it from local disks.  These
+ * IDs are used to later read the data (by the compute monotasks that perform the computation).
  */
 class ShuffleHelper[K, V, C](
     shuffleDependency: ShuffleDependency[K, V, C],
@@ -50,7 +51,7 @@ class ShuffleHelper[K, V, C](
   // TODO: Should this fetching of server statuses happen in a network monotask? Could
   //       involve network (to send message to the master); this should be measured.
   private val startTime = System.currentTimeMillis
-  private val statuses : Array[(BlockManagerId, Long)] =
+  private val statuses: Array[(BlockManagerId, Long)] =
     SparkEnv.get.mapOutputTracker.getServerStatuses(shuffleDependency.shuffleId, reduceId)
   logDebug("Fetching map output location for shuffle %d, reduce %d took %d ms".format(
     shuffleDependency.shuffleId, reduceId, System.currentTimeMillis - startTime))
@@ -60,22 +61,40 @@ class ShuffleHelper[K, V, C](
   private val blockIdToMapId = new HashMap[BlockId, Int]()
 
   def getReadMonotasks(): Seq[Monotask] = {
-    // Create NetworkMonotasks for all of the remote blocks with non-zero size.
-    val fetchMonotasks = new ArrayBuffer[NetworkMonotask]
-    for (((address, size), index) <- statuses.zipWithIndex) {
+    statuses.zipWithIndex.flatMap { statusAndIndex =>
+      val address = statusAndIndex._1._1
+      val size = statusAndIndex._1._2
+      val mapId = statusAndIndex._2
+      // Only need to fetch blocks that have non-zero size.
       if (size > 0) {
-        val blockId = new ShuffleBlockId(shuffleDependency.shuffleId, index, reduceId)
-        blockIdToMapId(blockId) = index
-        if (address.executorId == blockManager.blockManagerId.executorId) {
-          localBlockIds += blockId
+        val blockId = new ShuffleBlockId(shuffleDependency.shuffleId, mapId, reduceId)
+        blockIdToMapId(blockId) = mapId
+        val monotask = if (address.executorId == blockManager.blockManagerId.executorId) {
+          // Create a DiskReadMonotask to load the data into memory.
+          // The data loaded into memory by these monotasks will be automatically deleted by the
+          // LocalDagScheduler, because DiskReadMonotasks always marks the data read from disk as
+          // intermediate data that should be deleted when all of the monotasks's dependents
+          // complete.
+          // TODO: This assumes all shuffle data is stored on-disk; we'll need to update this
+          //       when we support reading shuffle data from memory.
+          blockManager.getBlockLoadMonotask(blockId, context).getOrElse {
+            throw new FetchFailedException(
+              blockManager.blockManagerId,
+              blockId.shuffleId,
+              blockId.mapId,
+              reduceId,
+              s"Could not find local shuffle block ID $blockId in BlockManager")
+          }
         } else {
-          val networkMonotask = new NetworkMonotask(context, address, blockId, size)
-          localBlockIds.append(networkMonotask.getResultBlockId())
-          fetchMonotasks.append(networkMonotask)
+          new NetworkMonotask(context, address, blockId, size)
         }
+
+        localBlockIds.append(monotask.getResultBlockId())
+        Some(monotask)
+      } else {
+        None
       }
     }
-    fetchMonotasks
   }
 
   def getDeserializedAggregatedSortedData(): Iterator[Product2[K, C]] = {
@@ -150,7 +169,8 @@ class ShuffleHelper[K, V, C](
             shuffleBlockId.shuffleId,
             shuffleBlockId.mapId,
             reduceId,
-            failureMessage)
+            failureMessage,
+            e)
       }
 
     case monotaskResultBlockId: MonotaskResultBlockId =>
