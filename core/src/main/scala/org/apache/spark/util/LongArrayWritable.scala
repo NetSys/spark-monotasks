d4df7334d10eea86c0394139ea5d2bdd9174072c
Kay Ousterhout <kayousterhout@gmail.com>
2015-09-14 18:04:03 -0700

    Added job that sorts random data from disk.
    The new job, contained in the SortJob class, first generates
    random key/value pairs and stores them on disk using Hadoop.
    Next, it repeatedly sorts the data, storing the output on disk.

---
+++ core/src/main/scala/org/apache/spark/util/LongArrayWritable.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2014 The Regents of The University California
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.util
+
+import java.io.{DataInput, DataOutput}
+
+import org.apache.hadoop.io.Writable
+
+/**
+ * Wrapper for an Array of Longs that implements the Writable class so that it can be written to
+ * Hadoop.
+ */
+class LongArrayWritable extends Writable {
+  private var data: Option[Array[Long]] = None
+
+  def this(data: Array[Long]) = {
+    this()
+    this.data = Some(data)
+  }
+
+  def get(): Array[Long] = {
+    data.get
+  }
+
+  override def write(out: DataOutput): Unit = {
+    val length = data.map(_.size).getOrElse(0)
+
+    out.writeInt(length)
+
+    data.map(_.foreach(out.writeLong(_)))
+  }
+
+  override def readFields(in: DataInput): Unit = {
+    val length = in.readInt()
+    data = Some(Array.fill(length)(in.readLong()))
+  }
+}
