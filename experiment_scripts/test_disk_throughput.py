800f8159aca13d7fea7ea892c0003de18d640d09
Kay Ousterhout <kayousterhout@gmail.com>
2015-09-16 00:38:55 +0000

    Added parallel read test.
    This commit adds two different read tests to test_disk_throughput.py:
    first, a test that reads each file individually; and second, a test
    that reads all 10 files at the same time in order to benchmark
    disk throughput when many threads are concurrently attempting to
    get data from disk.

--- experiment_scripts/test_disk_throughput.py
+++ experiment_scripts/test_disk_throughput.py
@@ -1,8 +1,11 @@
 import os
+import subprocess
+import threading
 import time
 
 def write_data_in_chunks(total_size):
-  filename = "/mnt/test_%s.txt" % total_size
+  start_time = time.time()
+  filename = "/mnt/test_%s_%s.txt" % (total_size, start_time)
   f = open(filename, "wb+")
   chunk = b'\xff'*4048
   for i in range(total_size / 4048):
@@ -10,14 +13,45 @@ def write_data_in_chunks(total_size):
   f.flush()
   os.fsync(f.fileno())
   f.close()
-  os.remove(filename)
+  write_end_time = time.time()
+  elapsed_time = write_end_time - start_time
+  print "  Written at rate ", float(total_size) / (megabyte_in_bytes * elapsed_time)
+
+  subprocess.check_call("/root/spark-ec2/clear-cache.sh", shell=True)
+  data = open(filename, "r").read(total_size)
+  print "  Read at rate ", float(total_size) / (megabyte_in_bytes * (time.time() - write_end_time))
+
+  return filename
+
+def do_read(filename):
+  f = open(filename, "r")
+  f.read(total_size)
+  f.close()
 
 if __name__ == "__main__":
   megabyte_in_bytes = 1024 * 1024
   for total_size in [10 * megabyte_in_bytes, 100 * megabyte_in_bytes]:
+    filenames = []
     print "Writing file with %s bytes to disk" % total_size
     for trial in range(1, 10):
-      start_time = time.time()
-      write_data_in_chunks(total_size)
-      elapsed_time = time.time() - start_time
-      print "  Written at rate ", float(total_size) / (megabyte_in_bytes * elapsed_time)
+      filenames.append(write_data_in_chunks(total_size))
+
+    subprocess.check_call("/root/spark-ec2/clear-cache.sh", shell=True)
+
+    # Read all of the files in parallel.
+    threads = []
+    start_time = time.time()
+    for filename in filenames:
+      thread = threading.Thread(target = lambda: do_read(filename))
+      thread.start()
+      threads.append(thread)
+
+    for thread in threads:
+      thread.join()
+
+    elapsed_time = time.time() - start_time
+    print " Parallel read at rate ", float(total_size) * 10 / (megabyte_in_bytes * elapsed_time)
+
+    for filename in filenames:
+      os.remove(filename)
+
