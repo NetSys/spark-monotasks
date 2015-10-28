NetSys/spark-monotasks

commit 2fd5bfbf7438428d22f0b789166916b3592be2e7

Throw error if multiple FileSplits per partition.
Currently, when reading HDFS files, Monotasks does not support splitting
an HDFS partition file so that it corresponds to multiple FileSplits
(and therefore multiple Spark partitions). There must be a one-to-one
mapping between HDFS partitions and Spark partitions. If this invariant
does not hold, then Monotasks will silently drop records. This is not a
fundamental property of the Monotasks design, we just have not
implemented support for multiple splits per HDFS partition yet.

This commit modifies NewHadoopRDD.getPartitions() in order to verify
that each split corresponds to an entire HDFS file partition.

sql/core/src/main/scala/org/apache/spark/sql/parquet/newParquet.scala
@@ -14,6 +14,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/*
+ * Copyright 2014 The Regents of The University California
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.parquet
 
 import java.io.IOException
@@ -502,7 +519,8 @@ private[sql] case class ParquetRelation2(
           val rawSplits = inputFormat.getSplits(jobContext)
 
           Array.tabulate[SparkPartition](rawSplits.size) { i =>
-            new NewHadoopPartition(id, i, rawSplits(i).asInstanceOf[InputSplit with Writable])
+            new NewHadoopPartition(
+              id, i, rawSplits(i).asInstanceOf[InputSplit with Writable], jobConf)
           }
         }
       }

core/src/main/scala/org/apache/spark/monotasks/disk/HdfsReadMonotask.scala
@@ -18,10 +18,7 @@ package org.apache.spark.monotasks.disk
 
 import java.nio.ByteBuffer
 
-import scala.reflect.classTag
-
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.mapreduce.lib.input.FileSplit
 
 import org.apache.spark.{Partition, SparkEnv, TaskContextImpl}
 import org.apache.spark.executor.DataReadMethod
@@ -40,14 +37,7 @@ private[spark] class HdfsReadMonotask(
     hadoopConf) {
 
   private val hadoopSplit =
-    sparkPartition.asInstanceOf[NewHadoopPartition].serializableHadoopSplit.value match {
-      case fileSplit: FileSplit =>
-        fileSplit
-      case otherSplit =>
-        throw new UnsupportedOperationException("Unsupported InputSplit: " +
-          s"${otherSplit.getClass().getName()}. HdfsReadMonotask only supports InputSplits of " +
-          s"type ${classTag[FileSplit]}.")
-    }
+    sparkPartition.asInstanceOf[NewHadoopPartition].serializableHadoopSplit.value
 
   pathOpt = Some(hadoopSplit.getPath())
   resultBlockId = Some(new MonotaskResultBlockId(taskId))

core/src/main/scala/org/apache/spark/rdd/BinaryFileRDD.scala
@@ -15,6 +15,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2014 The Regents of The University California
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.rdd
 
 import org.apache.hadoop.conf.{ Configurable, Configuration }
@@ -44,7 +60,8 @@ private[spark] class BinaryFileRDD[T](
     val rawSplits = inputFormat.getSplits(jobContext).toArray
     val result = new Array[Partition](rawSplits.size)
     for (i <- 0 until rawSplits.size) {
-      result(i) = new NewHadoopPartition(id, i, rawSplits(i).asInstanceOf[InputSplit with Writable])
+      result(i) =
+        new NewHadoopPartition(id, i, rawSplits(i).asInstanceOf[InputSplit with Writable], conf)
     }
     result
   }

core/src/main/scala/org/apache/spark/rdd/NewHadoopRDD.scala
@@ -38,7 +38,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 import scala.collection.mutable.{HashMap, HashSet}
-import scala.reflect.{classTag, ClassTag}
+import scala.reflect.ClassTag
 import scala.util.control.NonFatal
 
 import org.apache.hadoop.conf.{Configurable, Configuration}
@@ -60,10 +60,11 @@ import org.apache.spark.util.NextIterator
 private[spark] class NewHadoopPartition(
     rddId: Int,
     val index: Int,
-    @transient rawSplit: InputSplit with Writable)
+    @transient rawSplit: InputSplit with Writable,
+    @transient hadoopConf: Configuration)
   extends Partition {
 
-  val serializableHadoopSplit = new SerializableWritable(rawSplit)
+  val serializableHadoopSplit = new SerializableWritable(validateSplit(rawSplit, hadoopConf))
 
   // The BlockId used to cache this partition's serialized bytes in the BlockManager after it is
   // read from disk. This is required because the process of reading a block from disk and the
@@ -72,6 +73,47 @@ private[spark] class NewHadoopPartition(
   var serializedDataBlockId: Option[BlockId] = None
 
   override def hashCode(): Int = 41 * (41 + rddId) + index
+
+  /**
+   * Currently, the only type of `InputSplit` that Monotasks supports is a `FileSplit` that
+   * corresponds to an entire HDFS partition. This method verifies that `rawHadoopSplit` meets these
+   * criteria, or throws an exception if it doesn't. Returns `rawHadoopSplit` cast to a `FileSplit`.
+   */
+  private def validateSplit(
+      rawHadoopSplit: InputSplit,
+      localHadoopConf: Configuration): FileSplit = {
+    val hadoopFileSplit = rawHadoopSplit match {
+      case fileSplit: FileSplit with Writable =>
+        fileSplit
+      case _ =>
+        throw new UnsupportedOperationException("Unsupported InputSplit: " +
+          s"${rawHadoopSplit.getClass().getName()}. NewHadoopRDD only supports InputSplits of " +
+          s"type ${classOf[FileSplit].getName()}.")
+    }
+
+    // Retrieve the FileStatus object for the partition so that we can verify that the FileSplit
+    // corresponds to the entire partition.
+    val pathToPartitionFile = hadoopFileSplit.getPath()
+    val fileStatuses =
+      pathToPartitionFile.getFileSystem(localHadoopConf).listStatus(pathToPartitionFile)
+    val numFiles = fileStatuses.size
+    val pathString = pathToPartitionFile.toUri()
+    if (numFiles == 0) {
+      throw new SparkException(s"Path $pathString does not exist.")
+    }
+    if (numFiles > 1) {
+      throw new SparkException(s"Path $pathString should point to a single file, but it " +
+        s"actually points to a directory containing $numFiles files. Monotasks does not " +
+        "currently support reading multiple files from one macrotask.")
+    }
+
+    if (hadoopFileSplit.getLength() != fileStatuses.head.getLen()) {
+      throw new UnsupportedOperationException(s"FileSplit for $pathString does not correspond " +
+        "to the entire HDFS partition. Monotasks does not currently support multiple " +
+        "FileSplits per HDFS partition.")
+    }
+    hadoopFileSplit
+  }
 }
 
 /**
@@ -128,22 +170,15 @@ class NewHadoopRDD[K, V](
     }
 
     val rawSplits = inputFormat.getSplits(newJobContext(hadoopConf, jobId)).toArray
-    (0 until rawSplits.size).map(i =>
-      new NewHadoopPartition(id, i, rawSplits(i).asInstanceOf[InputSplit with Writable])).toArray
+    (0 until rawSplits.size).map(i => new NewHadoopPartition(
+      id, i, rawSplits(i).asInstanceOf[InputSplit with Writable], hadoopConf)).toArray
   }
 
   override def compute(
       sparkPartition: Partition,
       sparkTaskContext: TaskContext): InterruptibleIterator[(K, V)] = {
     val memoryStoreHadoopPartition = sparkPartition.asInstanceOf[NewHadoopPartition]
-    val hadoopSplit = memoryStoreHadoopPartition.serializableHadoopSplit.value match {
-        case fileSplit: FileSplit =>
-          fileSplit
-        case otherSplit =>
-          throw new UnsupportedOperationException("Unsupported InputSplit: " +
-            s"${otherSplit.getClass().getName()}. NewHadoopRDD only supports InputSplits of " +
-            s"type ${classTag[FileSplit]}.")
-    }
+    val hadoopSplit = memoryStoreHadoopPartition.serializableHadoopSplit.value
 
     val startPosition = hadoopSplit.getStart()
     val path = hadoopSplit.getPath()
@@ -282,7 +317,7 @@ private[spark] class WholeTextFileRDD(
     inputFormatClass: Class[_ <: WholeTextFileInputFormat],
     keyClass: Class[String],
     valueClass: Class[String],
-    @transient hadoopConf: Configuration,
+    @transient private val hadoopConf: Configuration,
     private val minPartitions: Int)
   extends NewHadoopRDD[String, String](sc, inputFormatClass, keyClass, valueClass, hadoopConf) {
 
@@ -297,7 +332,7 @@ private[spark] class WholeTextFileRDD(
     inputFormat.setMinPartitions(jobContext, minPartitions)
 
     val rawSplits = inputFormat.getSplits(jobContext).toArray
-    (0 until rawSplits.size).map(i =>
-      new NewHadoopPartition(id, i, rawSplits(i).asInstanceOf[InputSplit with Writable])).toArray
+    (0 until rawSplits.size).map(i => new NewHadoopPartition(
+      id, i, rawSplits(i).asInstanceOf[InputSplit with Writable], hadoopConf)).toArray
   }
 }

commit 154f2b7dfa9e68dcf57b943b6b2bd1996f6b1576

Fix task not serializable bug in newParquet
Commit 2fd5bfb introduced a "task not serializable" bug in newParquet by
passing the wrong JobConf object to the NewHadoopPartition constructor
when overriding NewHadoopRDD.getPartitions(). This commit fixes this bug
by using a JobConf that is local to NewHadoopRDD.

sql/core/src/main/scala/org/apache/spark/sql/parquet/newParquet.scala
@@ -520,7 +520,7 @@ private[sql] case class ParquetRelation2(
 
           Array.tabulate[SparkPartition](rawSplits.size) { i =>
             new NewHadoopPartition(
-              id, i, rawSplits(i).asInstanceOf[InputSplit with Writable], jobConf)
+              id, i, rawSplits(i).asInstanceOf[InputSplit with Writable], getConf)
           }
         }
       }