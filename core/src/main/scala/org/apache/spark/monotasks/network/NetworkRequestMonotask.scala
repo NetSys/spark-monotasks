kayousterhout/spark-1

Batch requests for shuffle data.
This commit changes the way that shuffle data is requested so that each each macrotask
sends one request per remote executor rather than one request per shuffle block. This commit
benefits performance because it eliminates harmful re-ordering that was occuring when shuffle
blocks were requested.  For the best performance, when executor A is sending shuffle data to
executor B, data for different tasks running on B should not be intermixed; instead, A should
send all of the data for a particular macrotask before beginning to send data for the next
macrotask. This commit guarantees that behavior in the case that all of the shuffle data is
in-memory: if executor A receives requests for data for two macrotasks, it will
receive all of the requests for blocks for one macrotask first (because all of the blocks are
described in a single request message) and the TransportRequestHandler will enqueue all of
the NetworkMonotasks for the response (by calling BlockManager.getBlockData) before handling
any responses for the other macrotask.

This differs from the current behavior: currently, we send one request for each shuffle block,
so executor A might receive requests for blocks for task 1 intermixed with requests for task 2,
so the responses would also be intermixed.

core/src/main/scala/org/apache/spark/monotasks/network/NetworkRequestMonotask.scala
@@ -16,6 +16,7 @@
 
 package org.apache.spark.monotasks.network
 
+import scala.collection.mutable.HashMap
 import scala.util.control.NonFatal
 
 import org.apache.spark.{FetchFailed, Logging, SparkEnv, SparkException, TaskContextImpl}
@@ -31,18 +32,15 @@ import org.apache.spark.util.Utils
  * data, and does not deserialize it.
  *
  * @param remoteAddress remote BlockManager to fetch from.
- * @param shuffleBlockId Id of the remote block to fetch.
- * @param size Estimated size of the data to fetch (used to keep track of how many bytes are in
- *             flight).
+ * @param shuffleBlockIdsAndSizes sequence of two-item tuples describing the shuffle blocks, where
+ *                                the first item is a BlockId and the second item is the block's
+ *                                approximate size.
  */
 private[spark] class NetworkRequestMonotask(
     context: TaskContextImpl,
     private val remoteAddress: BlockManagerId,
-    private val shuffleBlockId: ShuffleBlockId,
-    private val size: Long)
+    private val shuffleBlockIdsAndSizes: Seq[(ShuffleBlockId, Long)])
   extends NetworkMonotask(context) with Logging with BlockReceivedCallback {
-
-  resultBlockId = Some(new MonotaskResultBlockId(taskId))
 
   /** Scheduler to notify about bytes received over the network. Set by execute(). */
   private var networkScheduler: Option[NetworkScheduler] = None
@@ -50,28 +48,39 @@ private[spark] class NetworkRequestMonotask(
   /** Time when the request for remote data was issued. */
   private var requestIssueTimeNanos = 0L
 
+  val outstandingBlockIdToSize = new HashMap[BlockId, Long]
+  shuffleBlockIdsAndSizes.foreach {
+    case (shuffleBlockId, size) =>
+      outstandingBlockIdToSize.put(shuffleBlockId, size)
+  }
+
   override def execute(scheduler: NetworkScheduler): Unit = {
-    logInfo(s"Sending request for block $shuffleBlockId (size (${Utils.bytesToString(size)}}) " +
-      s"to $remoteAddress")
+    val blockIds = shuffleBlockIdsAndSizes.map(_._1)
+    val blockIdsAsString = blockIds.mkString(", ")
+    val totalSize = shuffleBlockIdsAndSizes.map(_._2).sum
+    logInfo(s"Monotask $taskId: sending request for blocks $blockIdsAsString (total size " +
+      s"(${Utils.bytesToString(totalSize)}}) to $remoteAddress")
     networkScheduler = Some(scheduler)
-    scheduler.addOutstandingBytes(size)
+    scheduler.addOutstandingBytes(totalSize)
     requestIssueTimeNanos = System.nanoTime()
 
     try {
-      SparkEnv.get.blockTransferService.fetchBlock(
+      SparkEnv.get.blockTransferService.fetchBlocks(
         remoteAddress.host,
         remoteAddress.port,
-        shuffleBlockId.toString,
+        blockIds.map(_.toString).toArray,
         context.taskAttemptId,
         context.attemptNumber,
         this)
     } catch {
       case NonFatal(t) => {
-        logError(s"Failed to initiate fetchBlock for shuffle block $shuffleBlockId", t)
+        logError(s"Failed to initiate fetchBlock for shuffle blocks $blockIdsAsString", t)
+        // Use the first shuffle block as the failure reason (only one fetch failure will be
+        // reported back to the master anyway).
         val failureReason = FetchFailed(
           remoteAddress,
-          shuffleBlockId.shuffleId,
-          shuffleBlockId.mapId,
+          blockIds(0).shuffleId,
+          blockIds(0).mapId,
           context.partitionId,
           Utils.exceptionString(t))
         handleException(failureReason)
@@ -79,16 +88,27 @@ private[spark] class NetworkRequestMonotask(
     }
   }
 
+  private def updateOutstandingBytes(blockIdStr: String, callbackMethodName: String): Unit = {
+    val blockId = BlockId(blockIdStr)
+    val size = outstandingBlockIdToSize.getOrElse(blockId,
+      throw new IllegalStateException(
+        s"$callbackMethodName called for block $blockId, which is not outstanding"))
+
+    networkScheduler.map(_.addOutstandingBytes(-size)).orElse {
+      throw new IllegalStateException(
+        s"$callbackMethodName called for block $blockId in monotask $taskId before a " +
+          s"NetworkScheduler was configured")
+    }
+    outstandingBlockIdToSize -= blockId
+  }
+
+  /** This method will be called multiple times, one for each block that was requested. */
   override def onSuccess(
       blockId: String,
       diskReadNanos: Long,
       totalRemoteNanos: Long,
       buf: ManagedBuffer): Unit = {
-    networkScheduler.map(_.addOutstandingBytes(-size)).orElse {
-      throw new IllegalStateException(
-        s"onSuccess called for block $blockId in monotask $taskId before a NetworkScheduler " +
-        "was configured")
-    }
+    updateOutstandingBytes(blockId, "onSuccess")
 
     val elapsedNanos = System.nanoTime() - requestIssueTimeNanos
     logInfo(s"Received block $blockId from BlockManagerId $remoteAddress (total elapsed nanos: " +
@@ -97,17 +117,17 @@ private[spark] class NetworkRequestMonotask(
     // Increment the ref count because we need to pass this to a different thread.
     // This needs to be released after use.
     buf.retain()
-    SparkEnv.get.blockManager.cacheSingle(getResultBlockId(), buf, StorageLevel.MEMORY_ONLY, false)
+    SparkEnv.get.blockManager.cacheSingle(
+      BlockId(blockId), buf, StorageLevel.MEMORY_ONLY, tellMaster = false)
     context.taskMetrics.incDiskNanos(diskReadNanos)
-    localDagScheduler.post(TaskSuccess(this))
+    if (outstandingBlockIdToSize.isEmpty) {
+      logInfo(s"Notifying LocalDagScheduler of completion of monotask $taskId")
+      localDagScheduler.post(TaskSuccess(this))
+    }
   }
 
   override def onFailure(failedBlockId: String, e: Throwable): Unit = {
-    networkScheduler.map(_.addOutstandingBytes(-size)).orElse {
-      throw new IllegalStateException(
-        s"onFailure called for block $failedBlockId in monotask $taskId before a " +
-        "NetworkScheduler was configured")
-    }
+    updateOutstandingBytes(failedBlockId, "onFailure")
 
     logError(s"Failed to get block(s) from ${remoteAddress.host}:${remoteAddress.port}", e)
     BlockId(failedBlockId) match {
@@ -124,6 +144,20 @@ private[spark] class NetworkRequestMonotask(
         val exception = new SparkException(
           s"Failed to get block $failedBlockId, which is not a shuffle block")
         handleException(exception)
+    }
+  }
+
+  /**
+   * NetworkRequestMonotask overrides cleanupIntermediateData because it needs to cleanup all
+   * of the network blocks fetched over the network.
+   */
+  override def cleanupIntermediateData(): Unit = {
+    super.cleanupIntermediateData()
+    val blockManager = SparkEnv.get.blockManager
+    // Delete all of the shuffle blocks that were fetched over the network.
+    shuffleBlockIdsAndSizes.foreach {
+      case (blockId, size) =>
+        blockManager.removeBlockFromMemory(blockId, tellMaster = false)
     }
   }
 }

NetSys/spark-monotasks

core/src/main/scala/org/apache/spark/monotasks/network/NetworkRequestMonotask.scala
@@ -16,14 +16,14 @@
 
 package org.apache.spark.monotasks.network
 
+import scala.collection.mutable.HashMap
 import scala.util.control.NonFatal
 
 import org.apache.spark.{FetchFailed, Logging, SparkEnv, SparkException, TaskContextImpl}
 import org.apache.spark.monotasks.TaskSuccess
 import org.apache.spark.network.buffer.ManagedBuffer
 import org.apache.spark.network.client.BlockReceivedCallback
-import org.apache.spark.storage.{BlockId, BlockManagerId, MonotaskResultBlockId, ShuffleBlockId,
-  StorageLevel}
+import org.apache.spark.storage.{BlockId, BlockManagerId, ShuffleBlockId, StorageLevel}
 import org.apache.spark.util.Utils
 
 /**
@@ -31,18 +31,15 @@ import org.apache.spark.util.Utils
  * data, and does not deserialize it.
  *
  * @param remoteAddress remote BlockManager to fetch from.
- * @param shuffleBlockId Id of the remote block to fetch.
- * @param size Estimated size of the data to fetch (used to keep track of how many bytes are in
- *             flight).
+ * @param shuffleBlockIdsAndSizes sequence of two-item tuples describing the shuffle blocks, where
+ *                                the first item is a BlockId and the second item is the block's
+ *                                approximate size.
  */
 private[spark] class NetworkRequestMonotask(
     context: TaskContextImpl,
     private val remoteAddress: BlockManagerId,
-    private val shuffleBlockId: ShuffleBlockId,
-    private val size: Long)
+    private val shuffleBlockIdsAndSizes: Seq[(ShuffleBlockId, Long)])
   extends NetworkMonotask(context) with Logging with BlockReceivedCallback {
-
-  resultBlockId = Some(new MonotaskResultBlockId(taskId))
 
   /** Scheduler to notify about bytes received over the network. Set by execute(). */
   private var networkScheduler: Option[NetworkScheduler] = None
@@ -50,28 +47,39 @@ private[spark] class NetworkRequestMonotask(
   /** Time when the request for remote data was issued. */
   private var requestIssueTimeNanos = 0L
 
+  val outstandingBlockIdToSize = new HashMap[BlockId, Long]
+  shuffleBlockIdsAndSizes.foreach {
+    case (shuffleBlockId, size) =>
+      outstandingBlockIdToSize.put(shuffleBlockId, size)
+  }
+
   override def execute(scheduler: NetworkScheduler): Unit = {
-    logInfo(s"Sending request for block $shuffleBlockId (size (${Utils.bytesToString(size)}}) " +
-      s"to $remoteAddress")
+    val blockIds = shuffleBlockIdsAndSizes.map(_._1)
+    val blockIdsAsString = blockIds.mkString(", ")
+    val totalSize = shuffleBlockIdsAndSizes.map(_._2).sum
+    logInfo(s"Monotask $taskId: sending request for blocks $blockIdsAsString (total size " +
+      s"(${Utils.bytesToString(totalSize)}}) to $remoteAddress")
     networkScheduler = Some(scheduler)
-    scheduler.addOutstandingBytes(size)
+    scheduler.addOutstandingBytes(totalSize)
     requestIssueTimeNanos = System.nanoTime()
 
     try {
-      SparkEnv.get.blockTransferService.fetchBlock(
+      SparkEnv.get.blockTransferService.fetchBlocks(
         remoteAddress.host,
         remoteAddress.port,
-        shuffleBlockId.toString,
+        blockIds.map(_.toString).toArray,
         context.taskAttemptId,
         context.attemptNumber,
         this)
     } catch {
       case NonFatal(t) => {
-        logError(s"Failed to initiate fetchBlock for shuffle block $shuffleBlockId", t)
+        logError(s"Failed to initiate fetchBlock for shuffle blocks $blockIdsAsString", t)
+        // Use the first shuffle block as the failure reason (only one fetch failure will be
+        // reported back to the master anyway).
         val failureReason = FetchFailed(
           remoteAddress,
-          shuffleBlockId.shuffleId,
-          shuffleBlockId.mapId,
+          blockIds(0).shuffleId,
+          blockIds(0).mapId,
           context.partitionId,
           Utils.exceptionString(t))
         handleException(failureReason)
@@ -79,16 +87,32 @@ private[spark] class NetworkRequestMonotask(
     }
   }
 
+  /**
+   * Removes the given block from outstandingBlockIdToSize (which is used to determine when this
+   * monotask has finished), and notifies the NetworkScheduler that the bytes in the given block are
+   * no longer outstanding.
+   */
+  private def removeOutstandingBlock(blockIdStr: String, callbackMethodName: String): Unit = {
+    val blockId = BlockId(blockIdStr)
+    val size = outstandingBlockIdToSize.getOrElse(blockId,
+      throw new IllegalStateException(
+        s"$callbackMethodName called for block $blockId, which is not outstanding"))
+
+    networkScheduler.map(_.addOutstandingBytes(-size)).orElse {
+      throw new IllegalStateException(
+        s"$callbackMethodName called for block $blockId in monotask $taskId before a " +
+          s"NetworkScheduler was configured")
+    }
+    outstandingBlockIdToSize -= blockId
+  }
+
+  /** This method will be called multiple times, one for each block that was requested. */
   override def onSuccess(
       blockId: String,
       diskReadNanos: Long,
       totalRemoteNanos: Long,
       buf: ManagedBuffer): Unit = {
-    networkScheduler.map(_.addOutstandingBytes(-size)).orElse {
-      throw new IllegalStateException(
-        s"onSuccess called for block $blockId in monotask $taskId before a NetworkScheduler " +
-        "was configured")
-    }
+    removeOutstandingBlock(blockId, "onSuccess")
 
     val elapsedNanos = System.nanoTime() - requestIssueTimeNanos
     logInfo(s"Received block $blockId from BlockManagerId $remoteAddress (total elapsed nanos: " +
@@ -97,17 +121,17 @@ private[spark] class NetworkRequestMonotask(
     // Increment the ref count because we need to pass this to a different thread.
     // This needs to be released after use.
     buf.retain()
-    SparkEnv.get.blockManager.cacheSingle(getResultBlockId(), buf, StorageLevel.MEMORY_ONLY, false)
+    SparkEnv.get.blockManager.cacheSingle(
+      BlockId(blockId), buf, StorageLevel.MEMORY_ONLY, tellMaster = false)
     context.taskMetrics.incDiskNanos(diskReadNanos)
-    localDagScheduler.post(TaskSuccess(this))
+    if (outstandingBlockIdToSize.isEmpty) {
+      logInfo(s"Notifying LocalDagScheduler of completion of monotask $taskId")
+      localDagScheduler.post(TaskSuccess(this))
+    }
   }
 
   override def onFailure(failedBlockId: String, e: Throwable): Unit = {
-    networkScheduler.map(_.addOutstandingBytes(-size)).orElse {
-      throw new IllegalStateException(
-        s"onFailure called for block $failedBlockId in monotask $taskId before a " +
-        "NetworkScheduler was configured")
-    }
+    removeOutstandingBlock(failedBlockId, "onFailure")
 
     logError(s"Failed to get block(s) from ${remoteAddress.host}:${remoteAddress.port}", e)
     BlockId(failedBlockId) match {
@@ -124,6 +148,20 @@ private[spark] class NetworkRequestMonotask(
         val exception = new SparkException(
           s"Failed to get block $failedBlockId, which is not a shuffle block")
         handleException(exception)
+    }
+  }
+
+  /**
+   * NetworkRequestMonotask overrides cleanupIntermediateData because it needs to cleanup all
+   * of the shuffle blocks fetched over the network.
+   */
+  override def cleanupIntermediateData(): Unit = {
+    super.cleanupIntermediateData()
+    val blockManager = SparkEnv.get.blockManager
+    // Delete all of the shuffle blocks that were fetched over the network.
+    shuffleBlockIdsAndSizes.foreach {
+      case (blockId, size) =>
+        blockManager.removeBlockFromMemory(blockId, tellMaster = false)
     }
   }
 }