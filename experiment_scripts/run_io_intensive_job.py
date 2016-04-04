99c0f87ef4a6f41959098b7dbc093fcbd6bc9c34
Max Wolffe <max.alan.wolffe@gmail.com>
2016-04-04 16:11:43 -0700

    Fix io_intensive_job to work on r2 machines
    This commit makes the run_io_intensive_job.py script use path names
    that are compatible with EC2 and the millennium machines.

--- experiment_scripts/run_io_intensive_job.py
+++ experiment_scripts/run_io_intensive_job.py
@@ -13,7 +13,7 @@ target_total_data_gb = 10
 # end up writing data to two different blocks, which we don't handle correctly.
 hdfs_blocks_per_gb = 1024 / 105
 
-slaves = [slave_line.strip("\n") for slave_line in open("/root/spark/conf/slaves").readlines()]
+slaves = utils.get_workers()
 print "Running experiment assuming slaves %s" % slaves
 
 num_machines = len(slaves)
@@ -34,11 +34,13 @@ use_existing_data_files = utils.check_if_hdfs_file_exists(data_filename)
 # The cores_per_worker parameter won't be used by the experiment; it's just included here for
 # convenience in how the log files are named.
 parameters = [num_tasks, num_tasks, items_per_task, values_per_key, num_shuffles,
-    data_filename, use_existing_data_files, cores_per_worker]
+              data_filename, use_existing_data_files, cores_per_worker]
 stringified_parameters = ["%s" % p for p in parameters]
-command = "/root/spark/bin/run-example monotasks.SortJob %s" % " ".join(stringified_parameters)
-print command
-subprocess.check_call(command, shell=True)
+command = utils.get_full_path("spark/bin/run-example")
+args = "monotasks.SortJob {}".format(" ".join(stringified_parameters))
+command_with_args = "{} {}".format(command, args)
+print command_with_args
+subprocess.check_call(command_with_args, shell=True)
 
 utils.copy_and_zip_all_logs(stringified_parameters, slaves)
 
