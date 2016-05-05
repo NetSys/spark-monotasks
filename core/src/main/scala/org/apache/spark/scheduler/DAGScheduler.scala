6aeba4650c52ce933a969783bb327b6f48687341
Kay Ousterhout <kayousterhout@gmail.com>
2016-05-04 18:25:30 -0700

    Immediately delete shuffle data.
    This commit modifies the DAGScheduler to immediately delete
    shuffle data when a stage that consumes the shuffle data
    finishes. This is to avoid issues where the shuffle data fills
    up disk or memory on the machines where the shuffle ran.

--- core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
+++ core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
@@ -952,6 +952,27 @@ class DAGScheduler(
       }
       listenerBus.post(SparkListenerStageCompleted(stage.latestInfo))
       runningStages -= stage
+
+      // Immediately delete shuffle data. This is to skirt issues with disks filling up when
+      // the context cleaner can't keep up.
+      // TODO: This code will lead to failures if multiple stages depend on the same shuffle
+      //       output.
+      val waitingForVisit = new Stack[RDD[_]]
+      waitingForVisit.push(stage.rdd)
+      while(!waitingForVisit.isEmpty) {
+        val currentRdd = waitingForVisit.pop()
+        currentRdd.dependencies.foreach {
+          case shuffleDependency: ShuffleDependency[_, _, _] =>
+            val shuffleId = shuffleDependency.shuffleId
+            logInfo(s"Deleting shuffle data for shuffle $shuffleId because $stage finished.")
+            mapOutputTracker.unregisterShuffle(shuffleId)
+            blockManagerMaster.removeShuffle(shuffleId, blocking = true)
+            logInfo(s"Done deleting shuffle data")
+
+          case dep: Dependency[_] =>
+            waitingForVisit.push(dep.rdd)
+        }
+      }
     }
     event.reason match {
       case Success =>
