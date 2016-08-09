kayousterhout/spark-1

Added logging about how long each monotask took.
This logging can be used to create plots showing when each monotask started
and stopped, to debug timing issues.

core/src/main/scala/org/apache/spark/monotasks/LocalDagScheduler.scala
@@ -277,8 +277,8 @@ private[spark] class LocalDagScheduler(
       completedMonotask: Monotask,
       serializedTaskResult: Option[ByteBuffer] = None): Unit = {
     val taskAttemptId = completedMonotask.context.taskAttemptId
-    logDebug(s"Monotask $completedMonotask (id: ${completedMonotask.taskId}) for " +
-      s"macrotask $taskAttemptId has completed.")
+    logDebug(s"$completedMonotask completed in ${completedMonotask.getRuntimeMillis()} ms " +
+      s"(started at ${completedMonotask.getStartTimeMillis}).")
     completedMonotask.cleanup()
     updateMetricsForFinishedMonotask(completedMonotask)
 

NetSys/spark-monotasks

core/src/main/scala/org/apache/spark/monotasks/LocalDagScheduler.scala
@@ -277,8 +277,8 @@ private[spark] class LocalDagScheduler(
       completedMonotask: Monotask,
       serializedTaskResult: Option[ByteBuffer] = None): Unit = {
     val taskAttemptId = completedMonotask.context.taskAttemptId
-    logDebug(s"Monotask $completedMonotask (id: ${completedMonotask.taskId}) for " +
-      s"macrotask $taskAttemptId has completed.")
+    logInfo(s"$completedMonotask completed in ${completedMonotask.getRuntimeMillis()} ms " +
+      s"(started at ${completedMonotask.getStartTimeMillis}).")
     completedMonotask.cleanup()
     updateMetricsForFinishedMonotask(completedMonotask)
 